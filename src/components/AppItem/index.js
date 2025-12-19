0df4cd7a288c0462b371e498a1fb613cb185acb7
Shamshuddin Shaik <shamshuddinis726@gmail.com>
2025-12-19 15:21:45 +0530

    All Test Cases Passed

--- src/components/AppItem/index.js
+++ src/components/AppItem/index.js
@@ -3,21 +3,13 @@ import './index.css'
 
 const AppItem = props => {
   const {appDetails} = props
-  const {appId, imageUrl, appName} = appDetails
+  const {appName, imageUrl} = appDetails
+
   return (
-    <>
-      <li className="project-item-container">
-        <img
-          className="project-item-image"
-          src={imageUrl}
-          alt={`project-item${appId}`}
-        />
-        <div className="project-item-details-container">
-          <h1 className="project-item-title">{appName}</h1>
-        </div>
-      </li>
-    </>
+    <li className="app-item">
+      <img className="app-image" src={imageUrl} alt={appName} />
+      <p className="app-name">{appName}</p>
+    </li>
   )
 }
-
 export default AppItem
